e0e6b3e5033073dec55acd7badb317cb6d9050f0
Carlos Arismendi <carlos.arismendisanchez@gmail.com>
2024-01-09 14:39:31 +0100

    First iteration of parallel.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BIN_FILE=1brc
-AMOUNT=1k
+AMOUNT=50m
 MEASUREMENTS_FILE=measurements_$(AMOUNT).txt
 ACTUAL_RESULTS_FILE=actual_results_$(AMOUNT).txt
 EXPECTED_RESULTS_FILE=expected_results_$(AMOUNT).txt
@@ -8,15 +8,13 @@ EXPECTED_RESULTS_FILE=expected_results_$(AMOUNT).txt
 build:
 	go build -o $(BIN_FILE)
 
-.PHONY: run
-time: build
-	export MEASUREMENTS_FILE=$(MEASUREMENTS_FILE) \
-		&& time ./$(BIN_FILE) > $(ACTUAL_RESULTS_FILE)
+.PHONY: time
+time: build	
+	MEASUREMENTS_FILE=$(MEASUREMENTS_FILE) /usr/bin/time -v ./$(BIN_FILE) 
 
 .PHONY: run
 run: build
-	export MEASUREMENTS_FILE=$(MEASUREMENTS_FILE) \
-		&& ./$(BIN_FILE) > $(ACTUAL_RESULTS_FILE)
+	MEASUREMENTS_FILE=$(MEASUREMENTS_FILE) ./$(BIN_FILE) > $(ACTUAL_RESULTS_FILE)
 
 .PHONY: test
 test: clean run
--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
+)
+
+const (
+	Workers  = 20
+	Reducers = 1
 )
 
 type Station struct {
@@ -37,13 +44,49 @@ func printResults(stationsMap map[string]*Station) {
 		return stations[i].Name < stations[j].Name
 	})
 
-	fmt.Print("{")
+	var sb strings.Builder
+	sb.WriteString("{")
 	sep := ""
 	for _, v := range stations {
-		fmt.Printf("%s%s=%.1f/%.1f/%.1f", sep, v.Name, v.Min, ((10*v.Sum)/float64(v.Count))/10, v.Max)
+		sb.WriteString(fmt.Sprintf("%s%s=%.1f/%.1f/%.1f", sep, v.Name, v.Min, ((10*v.Sum)/float64(v.Count))/10, v.Max))
 		sep = ", "
 	}
-	fmt.Print("}\n")
+	sb.WriteString("}")
+	fmt.Println(sb.String())
+}
+
+func worker(wg *sync.WaitGroup, input <-chan string, output chan<- *Station) {
+	defer wg.Done()
+
+	for line := range input {
+		name, temperature := processLine(line)
+		output <- &Station{
+			Name:  name,
+			Min:   temperature,
+			Max:   temperature,
+			Sum:   temperature,
+			Count: 1,
+		}
+	}
+}
+
+func reducer(wg *sync.WaitGroup, stationsMap map[string]*Station, input <-chan *Station) {
+	defer wg.Done()
+
+	for station := range input {
+		name := station.Name
+		temperature := station.Min
+
+		s, ok := stationsMap[name]
+		if ok {
+			s.Min = min(s.Min, temperature)
+			s.Max = max(s.Max, temperature)
+			s.Sum += temperature
+			s.Count++
+			continue
+		}
+		stationsMap[name] = station
+	}
 }
 
 func main() {
@@ -51,6 +94,25 @@ func main() {
 	if measurementsFile == "" {
 		panic("MEASUREMENTS_FILE environment variable not set")
 	}
+
+	stationsMap := make(map[string]*Station)
+
+	linesCh := make(chan string, 10000)
+	stationsCh := make(chan *Station, 1)
+
+	wgWorkers := &sync.WaitGroup{}
+	wgWorkers.Add(Workers)
+	for i := 0; i < Workers; i++ {
+		go worker(wgWorkers, linesCh, stationsCh)
+	}
+	log.Println("Workers started.")
+
+	wgReducers := &sync.WaitGroup{}
+	wgReducers.Add(Reducers)
+	for i := 0; i < Reducers; i++ {
+		go reducer(wgReducers, stationsMap, stationsCh)
+	}
+	log.Println("Reducers started.")
 
 	// Open the file
 	file, err := os.Open(measurementsFile)
@@ -62,23 +124,20 @@ func main() {
 	// Create a scanner to read the file
 	scanner := bufio.NewScanner(file)
 
-	stationsMap := make(map[string]*Station)
-
 	// Read the file line by line
 	for scanner.Scan() {
-		// Print the line
-		line := scanner.Text()
-		stationName, temperature := processLine(line)
-		s, ok := stationsMap[stationName]
-		if ok {
-			s.Min = min(s.Min, temperature)
-			s.Max = max(s.Max, temperature)
-			s.Sum += temperature
-			s.Count++
-			continue
-		}
-		stationsMap[stationName] = &Station{Name: stationName, Min: temperature, Max: temperature, Sum: temperature, Count: 1}
+		linesCh <- scanner.Text()
 	}
+
+	log.Println("Scanner finished.")
+
+	close(linesCh)
+	wgWorkers.Wait()
+	log.Println("Workers finished.")
+
+	close(stationsCh)
+	wgReducers.Wait()
+	log.Println("Reducers finished.")
 
 	// Check for errors
 	if err := scanner.Err(); err != nil {
@@ -86,4 +145,5 @@ func main() {
 	}
 
 	printResults(stationsMap)
+	fmt.Println("")
 }
