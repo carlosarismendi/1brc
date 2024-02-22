c38b3c8433e273e58a0c22573decec9a96176c30
Carlos Arismendi <carlos.arismendisanchez@gmail.com>
2024-02-22 16:44:09 +0100

    optimized parseFloat.

--- main_test.go
+++ main_test.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkParseFloat(b *testing.B) {
-	arr := []string{"1.1", "22.0", "33.8", "-4.0", "-85.7"}
-	b.Run("strconv.ParseFloat", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for _, v := range arr {
-				n, _ := strconv.ParseFloat(string(v), 64)
-				n = n
-			}
-		}
-	})
-
-	arr2 := [][]byte{[]byte("1.1"), []byte("22.0"), []byte("33.8"), []byte("-4.0"), []byte("-85.7")}
+	arr1 := [][]byte{[]byte("1.1"), []byte("22.0"), []byte("33.8"), []byte("-4.0"), []byte("-85.7")}
 	b.Run("parseFloat", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			for _, v := range arr2 {
+			for _, v := range arr1 {
 				n := parseFloat(v)
 				n = n
 			}
 		}
 	})
+
+	// arr2 := [][]byte{[]byte("1.1"), []byte("22.0"), []byte("33.8"), []byte("-4.0"), []byte("-85.7")}
+	// b.Run("parseFloat2", func(b *testing.B) {
+	// 	for i := 0; i < b.N; i++ {
+	// 		for _, v := range arr2 {
+	// 			n := parseFloat2(v)
+	// 			n = n
+	// 		}
+	// 	}
+	// })
 }
 
 func TestParseFloat(t *testing.T) {
--- utils.go
+++ utils.go
@@ -14,24 +14,21 @@ func processLine(nameBytes, tempBytes []byte) (station string, temperature float
 }
 
 func parseFloat(s []byte) float64 {
-	idx := 0
-	sign := int64(1)
+	var sign int64
+	var idx int
 	if s[0] == '-' {
 		sign = -1
 		idx = 1
+	} else {
+		sign = 1
 	}
 
 	num := int64(0)
 	for ; idx < len(s); idx++ {
 		if s[idx] == '.' {
-			idx++
-			break
+			continue
 		}
 
-		num = num*10 + int64(s[idx]-'0')
-	}
-
-	for ; idx < len(s); idx++ {
 		num = num*10 + int64(s[idx]-'0')
 	}
 
